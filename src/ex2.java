4aa254500de56e357488c67aa84ebdea4181e232
Isabela Zanette Martinello <isabela.213219@alunosatc.edu.br>
2025-08-07 20:14:52 -0300

    segundo commit

---
+++ src/ex2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        String nome = "Isabela";
+        int idade = 19;
+        double nota = 8.597;
+
+        System.out.printf("Nome: %s, Idade: %d\n", nome, idade);
+        System.out.printf("Nota Final: %.2f\n", nota);
+    }
+}
